6fdb0e79d11b70cff6e031df5dca9bbf9d4f44dd
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-06-22 16:08:42 +0500

    7

--- if07.py
+++ if07.py
@@ -13,13 +13,13 @@ def main(a):
         string: the message to print
     """
     if a%2==1 and a>0:
-        x = "odd"
+        x = "positive odd number"
     if a%2==0 and a>0:
-        x = "even"
+        x = "positive even number"
     if a%2==1 and a<0:
-        x = "negative odd"
+        x = "negative odd number"
     if a%2==0 and a<0:
-        x = "negative even"
+        x = "negative even number"
     if a==0:
         x = "the number is zero"
     return x
