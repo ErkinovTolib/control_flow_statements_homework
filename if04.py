a75be6e7cd1c7c630c5cb26f29210e2dff6326fa
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-06-23 10:07:31 +0500

    if04.py

--- if04.py
+++ if04.py
@@ -1,3 +1,6 @@
+from this import s
+
+
 def main(a,b,c):
     """
     Find how many positive numbers there are in the given numbers.
@@ -8,4 +11,12 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    s = 0
+    if a>0:
+       s+=1
+    if b>0:
+        s+=1
+    if c>0:
+        s+=1
+    return s
+    
